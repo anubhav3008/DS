ad016dc7145d719b1df58456ccefd3e2559cfccb
anubhav <anubhav.workemail@gmail.com>
2017-04-09 17:39:35 +0530

    adding iterative solution

--- src/com/anubhav/problems/tree/ZigZag.java
+++ src/com/anubhav/problems/tree/ZigZag.java
@@ -1,22 +1,27 @@
 package com.anubhav.problems.tree;
 
+import java.util.Stack;
+
 import com.anubhav.dataStructures.Tree;
 import com.anubhav.dataStructures.Models.TreeNode;
 import com.anubhav.dataStructures.constants.Constants;
 
 public class ZigZag {
-	
+
 	public static void main(String args[]){
 		TreeNode node =Tree.getCompleteTree();
 		boolean ltr=false;
 		for(int i=0;i<Constants.level;i++){
-			
+
 			TraverseZigZag(node, i,ltr);
 			ltr=!ltr;
-				
+
 		}
+		System.out.println();
 		
-		
+		TraverseZigZag(Tree.getCompleteTree());
+
+
 	}
 	/*
 	 * 
@@ -25,7 +30,7 @@ public class ZigZag {
 	 */
 	public static void TraverseZigZag(TreeNode node, int level, boolean ltr){
 		if(level==0)
-			System.out.print(node.getData());
+			System.out.print(node.getData()+"\t");
 		else
 		{
 			if(ltr){
@@ -36,12 +41,40 @@ public class ZigZag {
 			{
 				TraverseZigZag(node.getRight(), level-1, ltr);
 				TraverseZigZag(node.getLeft(), level-1, ltr);
-				
+
 			}
 		}
-				
-		
-		
+	}
+/*
+ * 
+ * 
+ * Iterative : use two stacks
+ */
+
+	public static void TraverseZigZag(TreeNode node){
+		Stack<TreeNode> rightToLeft=new Stack<TreeNode>();
+		Stack<TreeNode> leftToRight=new Stack<TreeNode>();
+
+		leftToRight.push(node);
+		while(!leftToRight.isEmpty() || !rightToLeft.isEmpty()){
+			while(!leftToRight.isEmpty()){
+				TreeNode popped=leftToRight.pop();
+				if(popped!=null){
+					System.out.print(popped.getData()+"\t");
+					if(popped.getRight()!=null)rightToLeft.push(popped.getRight());
+					if(popped.getLeft()!=null)rightToLeft.push(popped.getLeft());
+				}
+			}
+			while(!rightToLeft.empty()){
+				TreeNode popped=rightToLeft.pop();
+				if(popped!=null){
+					System.out.print(popped.getData()+"\t");
+					if(popped.getLeft()!=null)leftToRight.push(popped.getLeft());
+					if(popped.getRight()!=null)leftToRight.push(popped.getRight());
+				}
+			}
+		}
+
 	}
 
 }
